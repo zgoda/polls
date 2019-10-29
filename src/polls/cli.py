eb02eae350ea6979297bd84822340963e3cf11d5
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-10-29 23:41:20 +0100

    Load settings from CLI param

--- src/polls/cli.py
+++ src/polls/cli.py
@@ -1,13 +1,22 @@
+import os
+
 import click
 from dotenv import load_dotenv, find_dotenv
+from simple_settings import settings
 
 
 cli = click.Group()
+cli.ignore_unknown_options = True
 
 
 @cli.command()
-def run():
-    pass
+@click.option(
+    '--settings', '-s', 'settings_path', required=True,
+    help='path to settings file',
+)
+def run(settings_path):
+    os.environ.setdefault('SIMPLE_SETTINGS', settings_path)
+    print(settings.as_dict())
 
 
 def main():
