981051cf0a76fc79736f16abc1c61da14b090a16
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-11-03 14:40:20 +0100

    Poll and Option serialization

--- src/polls/resource/poll.py
+++ src/polls/resource/poll.py
@@ -1,9 +1,9 @@
-from flask import abort
+from flask import abort, request
 from flask_restful import Resource
 from pony.orm.core import ObjectNotFound
 
 from ..models import Poll
-from ..schema import poll_schema, polls_schema
+from ..schema import poll_schema, polls_schema, option_schema
 
 
 class PollResource(Resource):
@@ -14,6 +14,20 @@ class PollResource(Resource):
             return poll_schema.dump(poll)
         except ObjectNotFound:
             abort(404)
+
+    def patch(self, poll_id):
+        try:
+            poll = Poll[poll_id]
+        except ObjectNotFound:
+            abort(404)
+        doc = request.json
+        options_doc = doc.pop('options', [])
+        for key, value in doc.items():
+            setattr(poll, key, value)
+        for option_data in options_doc:
+            option = option_schema.load(option_data)
+            if option not in poll.options:
+                poll.options.add(option)
 
 
 class PollCollection(Resource):
--- src/polls/schema.py
+++ src/polls/schema.py
@@ -1,12 +1,19 @@
-from marshmallow import fields
+from marshmallow import fields, post_load
 
 from .ext import msmw
+from .models import Option, Poll
 
 
 class OptionSchema(msmw.Schema):
 
     class Meta:
-        fields = ('id', 'title', 'value')
+        fields = ('id', 'title', 'value', 'poll')
+
+    poll = fields.Nested('PollSchema', only=('id',))
+
+    @post_load
+    def make_option(self, data, **kwargs):
+        return Option(**data)
 
 
 class PollSchema(msmw.Schema):
@@ -16,6 +23,13 @@ class PollSchema(msmw.Schema):
 
     options = fields.Nested(OptionSchema, many=True)
 
+    @post_load
+    def make_poll(self, data, **kwargs):
+        return Poll(**data)
+
+
+option_schema = OptionSchema()
+options_schema = OptionSchema(many=True)
 
 poll_schema = PollSchema()
 polls_schema = PollSchema(many=True)
